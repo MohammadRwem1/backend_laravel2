498183c5dd859d727c1663b361fbbfadb65793c2
MohammadRwem1 <0mohammadrwem0@gmail.com>
2025-12-29 23:34:50 +0300

    fixing problems

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use App\Http\Controllers\Admin\AdminController;
 
 Route::middleware('admin.basic')
     ->prefix('admin')
+    ->name('admin.')
     ->group(function () {
 
-        Route::get('/users/pending', [AdminController::class, 'pendingUsers']);
-        Route::post('/users/{id}/approve', [AdminController::class, 'approve']);
-        Route::post('/users/{id}/reject', [AdminController::class, 'reject']);
+        Route::get('/users/pending', [AdminController::class, 'pendingUsers'])->name('users.pending');
+        Route::post('/users/{id}/approve', [AdminController::class, 'approve'])->name('users.approve');
+        Route::post('/users/{id}/reject', [AdminController::class, 'reject'])->name('users.reject');
 
     });
