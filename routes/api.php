9b72ed14b46d06d0fde20ae47f7be7e846a58c6b
MohammadRwem1 <0mohammadrwem0@gmail.com>
2025-12-30 23:23:03 +0300

    fixing problem

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ApartmentController;
+use App\Http\Controllers\BookingController;
 
 /*
 |--------------------------------------------------------------------------
